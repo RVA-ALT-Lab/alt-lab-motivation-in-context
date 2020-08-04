6183e2d5249949afa93fed434ab5314a1998b72c
Tom Woodward <twwoodward@me.com>
2020-08-04 14:45:31 -0400

    acf shifts

--- inc/acf.php
+++ inc/acf.php
@@ -163,7 +163,7 @@ function create_research_cpt() {
     'show_in_rest' => true,
     'publicly_queryable' => true,
     'capability_type' => 'post',
-    'menu_icon' => 'dashicons-universal-access-alt',
+    'menu_icon' => 'dashicons-welcome-learn-more',
   );
   register_post_type( 'research', $args );
   
