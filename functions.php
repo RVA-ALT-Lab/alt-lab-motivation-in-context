66b172ffb8231db04847d06b4cd2e7f7eb127532
Tom Woodward <twwoodward@me.com>
2020-08-05 15:35:44 -0400

    functioning project association

--- functions.php
+++ functions.php
@@ -198,41 +198,50 @@ add_action( 'save_post', 'motivation_name_slug_reorder', 10, 3 );
 function motivation_projects(){
 	global $post;
 	$html = '';
-	if( have_rows('project') ):
+	$projects = get_field('project_association', $post->ID);
+	if($projects){
 		$html .= '<div class="col-md-2 project-icon"><h2>Projects</h2><img src="'.  get_stylesheet_directory_uri() . '/imgs/project_clipboard.svg" class="img-fluid project-img" alt="Clipboard icon."></div>';
 		$html .= '<div class="col-md-10 project-info"><ul>';
+		foreach( $projects as $post ) {
+			$html .= '<li><h3>' . get_the_title() . '</h3></li>';
+		}
+	}
+
     // Loop through rows.
-	    while( have_rows('project') ) : the_row();
-
-	        // Load sub field value.
-	        $project_title = get_sub_field('project_title');
-	        $project_description = get_sub_field('project_description');
-	        $project_pi = get_sub_field('project_principal_investigator');
-	        $project_collaborators = get_sub_field('project_collaborators');
-	        $project_funders = get_sub_field('project_funding_partner');
-
-	        // Do something...
-	        $html .= '<li><h3>' . $project_title . '</h3>';
-	        $html .= '<div class="project-people">';
-	        if ($project_pi){
-	        	$html .= '<div class="proj-pi">PI: ' . $project_pi . '</div>';
-	        }
-	        if ($project_collaborators){
-	        	$html .= '<div class="proj-collab">Collaborators: ' . $project_collaborators . '</div>';
-	        }
-	         if ($project_funders){
-	        	$html .= '<div class="proj-funder">Project Funders: ' . $project_funders . '</div>';
-	        }
-	        $html .= '</div>' ;
-	        $html .= '<div class="proj-descripton">' . $project_description . '</div></li>';
-	    // End loop.
-	    endwhile;
-
-	// No value.
-	else :
-	    // Do something...
-	endif;
-	return $html . '</ul></div>';
+	 return $html . '</ul></div>';
 }
 
  //print("<pre>".print_r($a,true)."</pre>");}
+
+
+ //   while( have_rows('project') ) : the_row();
+
+	//         // Load sub field value.
+	//         $project_title = get_sub_field('project_title');
+	//         $project_description = get_sub_field('project_description');
+	//         $project_pi = get_sub_field('project_principal_investigator');
+	//         $project_collaborators = get_sub_field('project_collaborators');
+	//         $project_funders = get_sub_field('project_funding_partner');
+
+	//         // Do something...
+	//         $html .= '<li><h3>' . $project_title . '</h3>';
+	//         $html .= '<div class="project-people">';
+	//         if ($project_pi){
+	//         	$html .= '<div class="proj-pi">PI: ' . $project_pi . '</div>';
+	//         }
+	//         if ($project_collaborators){
+	//         	$html .= '<div class="proj-collab">Collaborators: ' . $project_collaborators . '</div>';
+	//         }
+	//          if ($project_funders){
+	//         	$html .= '<div class="proj-funder">Project Funders: ' . $project_funders . '</div>';
+	//         }
+	//         $html .= '</div>' ;
+	//         $html .= '<div class="proj-descripton">' . $project_description . '</div></li>';
+	//     // End loop.
+	//     endwhile;
+
+	// // No value.
+	// else :
+	//     // Do something...
+	// endif;
+	// return $html . '</ul></div>';
