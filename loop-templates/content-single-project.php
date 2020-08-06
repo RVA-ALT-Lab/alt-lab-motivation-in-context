b9bec655fc02e88794788831c5af6df9001f9136
Tom Woodward <twwoodward@me.com>
2020-08-06 09:47:43 -0400

    lots of project shifts

--- loop-templates/content-single-project.php
+++ loop-templates/content-single-project.php
@@ -21,15 +21,32 @@ defined( 'ABSPATH' ) || exit;
 
 	<div class="entry-content">
 		<div class="row">
-			<div class="col-md-4">
+			<div class="col-md-3">
 			</div>
-			<div class="col-md-8 proj-details">
-				<div class="pi-area"><span class="proj-cat">Principal Investigator:</span> <?php the_field('project_principal_investigator');?></div>
-				<div class="collab-area"><span class="proj-cat">Collaborators:</span> <?php the_field('project_collaborators');?></div>
-				<div class="funding-area"><span class="proj-cat">Funding Partners:</span> <?php the_field('project_funding_partner');?></div>
-				<div class="description-area">
-					<span class="proj-cat">Project Description:</span> 
-					<?php the_content();?>
+			<div class="col-md-9 proj-details">
+				<div class="pi-area proj-data-line">
+					<div class="proj-cat proj-label">Principal Investigator:</div> 
+					<div class="proj-data">
+						<?php the_field('project_principal_investigator');?>						
+					</div>
+				</div>
+				<div class="collab-area proj-data-line">
+					<div class="proj-cat proj-label">Collaborators:</div>
+					<div class="proj-data">
+						<?php the_field('project_collaborators');?>
+					</div>
+				</div>
+				<div class="funding-area proj-data-line">
+					<div class="proj-cat proj-label">Funding Partners:</div> 
+					<div class="proj-data">
+						<?php the_field('project_funding_partner');?>
+					</div>
+				</div>
+				<div class="description-area proj-data-line">
+					<div class="proj-cat proj-label">Project Description:</div> 
+					<div class="proj-data proj-description">
+						<?php the_content();?>
+					</div>
 				</div>
 			</div>
 		</div>
