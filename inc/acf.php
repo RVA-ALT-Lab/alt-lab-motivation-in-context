374d225f7943f00c06ba83bbcff4075b2dc4d0a1
Tom Woodward <twwoodward@me.com>
2020-08-07 15:13:37 -0400

    remove research as cpt

--- inc/acf.php
+++ inc/acf.php
@@ -112,66 +112,66 @@ add_action( 'init', 'create_person_cpt', 0 );
 // Register Custom Post Type research
 // Post Type Key: research
 
-function create_research_cpt() {
+// function create_research_cpt() {
 
-  $labels = array(
-    'name' => __( 'research', 'Post Type General Name', 'textdomain' ),
-    'singular_name' => __( 'Research', 'Post Type Singular Name', 'textdomain' ),
-    'menu_name' => __( 'Research', 'textdomain' ),
-    'name_admin_bar' => __( 'Research', 'textdomain' ),
-    'archives' => __( 'Research Archives', 'textdomain' ),
-    'attributes' => __( 'Research Attributes', 'textdomain' ),
-    'parent_item_colon' => __( 'Research:', 'textdomain' ),
-    'all_items' => __( 'All research', 'textdomain' ),
-    'add_new_item' => __( 'Add New Research', 'textdomain' ),
-    'add_new' => __( 'Add New', 'textdomain' ),
-    'new_item' => __( 'New Research', 'textdomain' ),
-    'edit_item' => __( 'Edit Research', 'textdomain' ),
-    'update_item' => __( 'Update Research', 'textdomain' ),
-    'view_item' => __( 'View Research', 'textdomain' ),
-    'view_items' => __( 'View research', 'textdomain' ),
-    'search_items' => __( 'Search research', 'textdomain' ),
-    'not_found' => __( 'Not found', 'textdomain' ),
-    'not_found_in_trash' => __( 'Not found in Trash', 'textdomain' ),
-    'featured_image' => __( 'Featured Image', 'textdomain' ),
-    'set_featured_image' => __( 'Set featured image', 'textdomain' ),
-    'remove_featured_image' => __( 'Remove featured image', 'textdomain' ),
-    'use_featured_image' => __( 'Use as featured image', 'textdomain' ),
-    'insert_into_item' => __( 'Insert into research', 'textdomain' ),
-    'uploaded_to_this_item' => __( 'Uploaded to this research', 'textdomain' ),
-    'items_list' => __( 'Research list', 'textdomain' ),
-    'items_list_navigation' => __( 'Research list navigation', 'textdomain' ),
-    'filter_items_list' => __( 'Filter Research list', 'textdomain' ),
-  );
-  $args = array(
-    'label' => __( 'research', 'textdomain' ),
-    'description' => __( '', 'textdomain' ),
-    'labels' => $labels,
-    'menu_icon' => '',
-    'supports' => array('title', 'editor', 'revisions', 'author', 'trackbacks', 'custom-fields', 'thumbnail',),
-    'taxonomies' => array('category'),
-    'public' => true,
-    'show_ui' => true,
-    'show_in_menu' => true,
-    'menu_position' => 5,
-    'show_in_admin_bar' => true,
-    'show_in_nav_menus' => true,
-    'can_export' => true,
-    'has_archive' => true,
-    'hierarchical' => false,
-    'exclude_from_search' => false,
-    'show_in_rest' => true,
-    'publicly_queryable' => true,
-    'capability_type' => 'post',
-    'menu_icon' => 'dashicons-welcome-learn-more',
-  );
-  register_post_type( 'research', $args );
+//   $labels = array(
+//     'name' => __( 'research', 'Post Type General Name', 'textdomain' ),
+//     'singular_name' => __( 'Research', 'Post Type Singular Name', 'textdomain' ),
+//     'menu_name' => __( 'Research', 'textdomain' ),
+//     'name_admin_bar' => __( 'Research', 'textdomain' ),
+//     'archives' => __( 'Research Archives', 'textdomain' ),
+//     'attributes' => __( 'Research Attributes', 'textdomain' ),
+//     'parent_item_colon' => __( 'Research:', 'textdomain' ),
+//     'all_items' => __( 'All research', 'textdomain' ),
+//     'add_new_item' => __( 'Add New Research', 'textdomain' ),
+//     'add_new' => __( 'Add New', 'textdomain' ),
+//     'new_item' => __( 'New Research', 'textdomain' ),
+//     'edit_item' => __( 'Edit Research', 'textdomain' ),
+//     'update_item' => __( 'Update Research', 'textdomain' ),
+//     'view_item' => __( 'View Research', 'textdomain' ),
+//     'view_items' => __( 'View research', 'textdomain' ),
+//     'search_items' => __( 'Search research', 'textdomain' ),
+//     'not_found' => __( 'Not found', 'textdomain' ),
+//     'not_found_in_trash' => __( 'Not found in Trash', 'textdomain' ),
+//     'featured_image' => __( 'Featured Image', 'textdomain' ),
+//     'set_featured_image' => __( 'Set featured image', 'textdomain' ),
+//     'remove_featured_image' => __( 'Remove featured image', 'textdomain' ),
+//     'use_featured_image' => __( 'Use as featured image', 'textdomain' ),
+//     'insert_into_item' => __( 'Insert into research', 'textdomain' ),
+//     'uploaded_to_this_item' => __( 'Uploaded to this research', 'textdomain' ),
+//     'items_list' => __( 'Research list', 'textdomain' ),
+//     'items_list_navigation' => __( 'Research list navigation', 'textdomain' ),
+//     'filter_items_list' => __( 'Filter Research list', 'textdomain' ),
+//   );
+//   $args = array(
+//     'label' => __( 'research', 'textdomain' ),
+//     'description' => __( '', 'textdomain' ),
+//     'labels' => $labels,
+//     'menu_icon' => '',
+//     'supports' => array('title', 'editor', 'revisions', 'author', 'trackbacks', 'custom-fields', 'thumbnail',),
+//     'taxonomies' => array('category'),
+//     'public' => true,
+//     'show_ui' => true,
+//     'show_in_menu' => true,
+//     'menu_position' => 5,
+//     'show_in_admin_bar' => true,
+//     'show_in_nav_menus' => true,
+//     'can_export' => true,
+//     'has_archive' => true,
+//     'hierarchical' => false,
+//     'exclude_from_search' => false,
+//     'show_in_rest' => true,
+//     'publicly_queryable' => true,
+//     'capability_type' => 'post',
+//     'menu_icon' => 'dashicons-welcome-learn-more',
+//   );
+//   register_post_type( 'research', $args );
   
-  // flush rewrite rules because we changed the permalink structure
-  global $wp_rewrite;
-  $wp_rewrite->flush_rules();
-}
-add_action( 'init', 'create_research_cpt', 0 );
+//   // flush rewrite rules because we changed the permalink structure
+//   global $wp_rewrite;
+//   $wp_rewrite->flush_rules();
+// }
+// add_action( 'init', 'create_research_cpt', 0 );
 
 
 
